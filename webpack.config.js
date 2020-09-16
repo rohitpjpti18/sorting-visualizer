33ea5376144c7cb7549123cc48abfcc46ca4f842
pritiprajapati314 <pritiprajapati314@gmail.com>
2020-09-16 17:18:51 +0530

    reorganised file systems

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: './App.ts',
+    },
+    resolve:{
+        extensions: [".webpack.js", ".web.js", ".js", ".ts"]
+    },
+    output:{
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader'
+            }
+        ]
+    }
+}
