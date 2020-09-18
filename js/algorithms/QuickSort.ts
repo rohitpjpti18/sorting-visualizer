0c9dec6e442a8414f407bc9c7ec1667d3542c06b
Rohit Prajapati <51499109+rohitpjpti18@users.noreply.github.com>
2020-09-18 10:31:34 +0530

    updated quicksort

---
+++ js/algorithms/QuickSort.ts
@@ -0,0 +1,125 @@
+import { textSpanIntersectsWithPosition } from "typescript";
+import Board from "../Board";
+/* async function quickSort(){
+    disableAllButton();
+    await quickSortActual(0, 49);
+
+    await traverse();
+    enableAllButton();
+}
+
+
+async function quickSortActual(start, end) { 
+    if(start >= end) { 
+        return; 
+    } 
+    let index = await partition(start, end);  
+      
+    await Promise.all([quickSortActual(start, index-1), 
+            quickSortActual(index+1, end)]);
+} 
+  
+
+async function partition(start, end) { 
+    let pivotIndex = start; 
+    let pivotValue = newArr[end]; 
+    
+    await checkBarSingle(end);
+    for(let i = start; i < end; i++) {
+        await checkBarSingle(i); 
+        if(newArr[i] > pivotValue) { 
+            //swap(arr, i, pivotIndex); 
+            var temp = newArr[i];
+            newArr[i] = newArr[pivotIndex];
+            newArr[pivotIndex] = temp;
+            await swapBar(i, pivotIndex); 
+            await nowCorrectBar(i, pivotIndex);
+            await nowNormalBar(i, pivotIndex);
+            pivotIndex++;
+        } 
+        await nowNormalBarSingle(i);
+    } 
+      
+    //await swap(arr, pivotIndex, end); 
+    var temp = newArr[end];
+    newArr[end] = newArr[pivotIndex];
+    newArr[pivotIndex] = temp;
+    await swapBar(end, pivotIndex);
+    await nowCorrectBar(end, pivotIndex);
+    await nowNormalBar(end, pivotIndex);
+      
+    return pivotIndex; 
+}  */
+
+import Color from "../Color";
+
+
+class QuickSort{
+    board:Board;
+    colorhandler:Color;
+
+    constructor(board:Board, colorhandler:Color){
+        this.board = board;
+        this.colorhandler = colorhandler;
+    }
+
+    async quickSortAlgorithm(start:number, end:number) { 
+        if(start >= end) { 
+            return; 
+        } 
+        let index = await this.partition(start, end);  
+          
+        await this.quickSortAlgorithm(start, index-1); 
+        await this.quickSortAlgorithm(index+1, end);
+
+    } 
+
+
+    async partition(start:number, end:number) { 
+        let pivotIndex = start; 
+        let pivotValue = this.board.numArray[end]; 
+        
+        await this.colorhandler.setColor(end, 1);
+        for(let i = start; i < end; i++) {
+            await this.colorhandler.setColor(i, 1); 
+            if(this.board.numArray[i] > pivotValue) { 
+                //swap(arr, i, pivotIndex);
+                this.board.swap(i, pivotIndex); 
+                await this.colorhandler.arrangeTwoElements(
+                    i,
+                    this.board.numArray[i],
+                    pivotIndex,
+                    this.board.numArray[pivotIndex]
+                );
+                await this.colorhandler.set2IndexToOrdered(i, pivotIndex);
+                await this.colorhandler.set2IndexToDefault(i, pivotIndex);
+                pivotIndex++;
+            } 
+
+            await this.colorhandler.setColor(i, 0);
+        } 
+          
+        //await swap(arr, pivotIndex, end); 
+        this.board.swap(end, pivotIndex);
+        await this.colorhandler.arrangeTwoElements(
+            end,
+            this.board.numArray[end],
+            pivotIndex,
+            this.board.numArray[pivotIndex]
+        );
+        await this.colorhandler.set2IndexToOrdered(end, pivotIndex);
+        await this.colorhandler.set2IndexToDefault(end, pivotIndex);
+          
+        return pivotIndex; 
+    }
+
+    async execute(){
+        this.board.sortInProgress = true;
+
+        await this.quickSortAlgorithm(0, this.board.numArray.length-1);
+        
+        this.board.sortInProgress = false;
+    }
+}
+
+export default QuickSort;
