7c9b649168c9d8d7a8a87f20a9cefd12a334e3fe
Rohit Prajapati <51499109+rohitpjpti18@users.noreply.github.com>
2020-09-18 10:32:24 +0530

    modified board

--- js/App.ts
+++ js/App.ts
@@ -2,17 +2,26 @@ import { formatDiagnosticsWithColorAndContext } from 'typescript';
 import Board from './Board';
 import Color from './Color';
 
-const arraySizes = [5, 10, 20, 25, 50, 100, 200]
-const sortSpeeds = [1000, 400, 200, 100, 50, 20, 5]
+const arraySizes = [5, 10, 20, 25, 50, 100, 150]
+const sortSpeeds = [1000, 500, 250, 100, 50, 25, 12, 6, 2, 1]
+
+var start = document.getElementById("start");
+
+var bubble = document.getElementById("bubble");
+var selection = document.getElementById("selection");
+var quick = document.getElementById("quick");
 
 var arraySizeInput = document.getElementById("arraySize");
 var sortSpeedInput = document.getElementById("sortSpeed");
 var generateArray = document.getElementById("generateArray");
 
-let board = new Board(25);
+
 let color = new Color(sortSpeeds[2]);
+let board = new Board(25, color);
 
 arraySizeInput.oninput = function(e){
+    if(board.sortInProgress) return;
+
     let size = parseInt((<HTMLInputElement>e.target).value);
     board.updateSize(arraySizes[size]);
 }
@@ -22,7 +31,38 @@ sortSpeedInput.oninput = function(e){
     color.updateSpeed(sortSpeeds[speed]);
 }
 
-
 generateArray.onclick = function(){
+    if(board.sortInProgress) return;
+
     board.generateArray();
+}
+
+
+
+
+bubble.onclick = function(){
+    if(board.sortInProgress) return;
+
+    board.algoID = 0;
+}
+
+selection.onclick = function(){
+    if(board.sortInProgress) return;
+
+    board.algoID = 1;
+}
+
+quick.onclick = function(){
+    if(board.sortInProgress) return;
+
+    board.algoID = 4;
+}
+
+
+
+
+start.onclick = function(){
+    if(board.sortInProgress) return;
+
+    board.startSorting();
 }
--- js/Board.ts
+++ js/Board.ts
@@ -1,23 +1,43 @@
-import RandomNumbers from "./utilities/RandomNumbers";
+/*
+Algorithm Name          Id
+Bubble Sort             0
+Insertion Sort          1
+Selection Sort          2
+Merge Sort              3
+Quick Sort              4
+Heap Sort               5
+*/
 
+import RandomNumbers from "./utilities/RandomNumbers";
+import BubbleSort from './algorithms/BubbleSort';
+import Color from "./Color";
+import QuickSort from "./algorithms/QuickSort";
 
 class Board{
     numArray: number[];
     size: number;
     random: RandomNumbers;
-    columnWidth: number
+    columnWidth: number;
 
-    constructor(size:number){
+    sortInProgress: boolean;
+    algoID:number;
+    colorHandler:Color;
+
+    constructor(size:number, colorhandler:Color){
         this.size = size;
         this.random = new RandomNumbers();
         this.numArray = [];
         this.columnWidth = 100/size;
+        this.algoID = 0;
+        this.colorHandler = colorhandler;
 
         this.generateArray();
+
+        this.sortInProgress = false;
     }
 
 
-    generateNewBar(num:number, id:number){
+    generateNewBar(num:number, id:number, size:number){
         let container = document.getElementById("main-container");
         let newCol = document.createElement("div");
         newCol.className = 'column';
@@ -26,6 +46,15 @@ class Board{
         let newBar = document.createElement("div");
         newBar.className = 'bar';
         newBar.id = id.toString();
+        if(size>50 && size<101){
+            newBar.style.width = '75%';
+        }
+        else if(size>100){
+            newBar.style.width = '67%';
+        }
+        else if(size>19 && size<51){
+            newBar.style.width = '90%';
+        }
         newBar.style.height = (num*10).toString()+'px';
 
         newCol.appendChild(newBar);
@@ -42,7 +71,7 @@ class Board{
         for(let i=0; i<this.size; i++){
             let newNum = this.random.GenerateRandomNumber(2, 50);
             this.numArray.push(newNum);
-            this.generateNewBar(newNum, i);
+            this.generateNewBar(newNum, i, this.size);
         }
     }
 
@@ -50,6 +79,30 @@ class Board{
         this.size = size;
         this.columnWidth = 100/size;
         this.generateArray();
+    }
+
+    swap(index1:number, index2:number){
+        var temp = this.numArray[index1];
+        this.numArray[index1] = this.numArray[index2];
+        this.numArray[index2] = temp;
+    }
+
+
+    async startSorting(){
+        switch(this.algoID){
+            case 0:
+                let bubbleSort = new BubbleSort(this, this.colorHandler);
+                await bubbleSort.execute();
+                await this.colorHandler.traverse(this.numArray);
+                break;
+            case 4:
+                let quicksort = new QuickSort(this, this.colorHandler);
+                await quicksort.execute();
+                await this.colorHandler.traverse(this.numArray);
+                break;
+            default:
+                console.log("no algorithm selected");
+        }
     }
 }
 
